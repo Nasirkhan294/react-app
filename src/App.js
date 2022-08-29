f3b269caf452789bb471ba54f88587049743e3ac
Nasirkhan294 <nasirbhatti.nk@gmail.com>
2022-08-29 21:39:12 +0500

    react-app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import { FaSearch } from 'react-icons/fa'
+import { FaRegBell } from 'react-icons/fa'
+import { FaReact} from 'react-icons/fa'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='container'>
+      <header>
+        <FaSearch  className='icon'/>
+        <p className='logo'><FaReact />React App</p>
+        <FaRegBell className='icon'/>
       </header>
+      <main>
+        <h3>Earnings</h3>
+        <table>
+          <tbody>
+            <tr>
+              <td>Total Plan Value</td>
+              <td>$300</td>
+            </tr>
+            <tr>
+              <td>Referral Commission</td>
+              <td>$0</td>
+            </tr>
+          </tbody>
+          <tbody>
+            <tr>
+              <td>Today's Profit</td>
+              <td>$0.018</td>
+            </tr>
+            <tr>
+              <td>Total Amount</td>
+              <td>$600</td>
+            </tr>
+          </tbody>
+        </table>
+      </main>
     </div>
   );
 }
